d1b3aaa9ec25bad55093816507b17ab09362f17d
izzah488 <izzahaina71@gmail.com>
2026-04-04 13:57:07 +0800

    fix port

--- nurqalby_1/Backend-1/app/main.py
+++ nurqalby_1/Backend-1/app/main.py
@@ -86,5 +86,5 @@ def recommend(data: UserInput):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
